7fa41b0c2940b703ea05b0ad85d3d7f4f2775c50
Yusuf qamber <yousif02.qamber@gmail.com>
2025-07-13 11:31:44 +0300

    creating home route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Here is where we import modules
 // We begin by loading Express
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
+app.get("/", (req, res) => {
+  res.send("working!");
+});
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
