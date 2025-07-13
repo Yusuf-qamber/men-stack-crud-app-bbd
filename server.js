b7662d3b7a8aa4820348b08cf5ba548e5a55eb9d
Yusuf qamber <yousif02.qamber@gmail.com>
2025-07-13 16:09:03 +0300

    adding index.ejs

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // Here is where we import modules
 // We begin by loading Express
 const express = require("express");
-
 const app = express();
+const path = require("path");
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("working!");
+  res.render("index.ejs");
 });
 
 app.listen(3000, () => {
