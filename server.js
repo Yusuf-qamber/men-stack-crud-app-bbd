feb6f0c88bd609cf0063b4841ba5559cc138b6b7
Yusuf qamber <yousif02.qamber@gmail.com>
2025-07-14 09:58:52 +0300

    connecting to the database

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-// Here is where we import modules
-// We begin by loading Express
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const path = require("path");
+
+//DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log(`Conected to MongoDB${mongoose.connection.name}`);
+});
 
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, "public")));
